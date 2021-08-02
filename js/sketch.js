ae29914f62bc60ecbfef52b412a088cb81e65f06
sarahkim711 <54784718+sarahkim711@users.noreply.github.com>
2021-08-02 12:07:40 -0500

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -21,8 +21,6 @@ function setup() {
 
   // print(random(300));
 
-  ellipseX= random(ellipseX, windowWidth);
-  ellipseY= random(ellipseY, windowHeight);
 
 
 }
@@ -39,32 +37,37 @@ function draw() {
 // rect(500,400,200,200);
 frameRate(.8);
   background(random(255),random(150),random(255));
+
+  ellipseX= random(ellipseX, windowWidth);
+  ellipseY= random(ellipseY, windowHeight);
 // ellipse(mouseX, mouseY, 10,10);
-fill(random(255));
-stroke(30);
-strokeWeight(30);
+
 // line(30,30, 30,300);
 // stroke(100,200,300);
 
 
-let type = 'Hello World!...compiling...';
-fill(50);
-text(type, 10, 10, 70, 80);
+
+
 
 // let a = createA('https://sarahkim711.github.io/iampolygon/', 'I Am Polygon');
 // a.position(windowWidth/4, windowHeight/4);
 
-// if (a > windowHeight || a < 0){
-// aYspeed= aYspeed * -1;
-// }
+if (a > windowHeight || a < 0){
+aYspeed= aYspeed * -1;
+}
 
 
 // let b = createA('https://sarahkim711.github.io/p5jsProject/', 'MyAdPreference();');
 // b.position(windowWidth/2, windowHeight/2);
-
+//
 // let c = createA('https://sites.google.com/artic.edu/vr-so-far/jacking-in', '...All my VR works...');
 // c.position(windowWidth/3, windowHeight/3);
 
+textSize(32);
+text('Hello! This website is still under construction :D', windowWidth/2, 30);
+fill(255);
+stroke(10);
+strokeWeight(5);
 // point(mouseX, mouseY);
 // point(pointX, pointY);
 // strokeWeight(20);
